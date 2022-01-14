cea849f9eb2c646787334c5e23acad61a8ceb360
coscoaj1 <coscoanthony1@gmail.com>
2022-01-14 18:06:31 -0500

    update/delete fields

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,7 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
 
-import { Table, Thead, Tr, Th, Tbody, Td, Box } from "@chakra-ui/react";
+import {
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  Box,
+  IconButton,
+} from "@chakra-ui/react";
 
 export default function Inventory() {
   const [data, setData] = useState();
@@ -13,6 +23,16 @@ export default function Inventory() {
         <Td>{item.sku}</Td>
         <Td>{item.location}</Td>
         <Td>{item.count}</Td>
+        <Td>
+          <IconButton>
+            <EditIcon />
+          </IconButton>
+        </Td>
+        <Td>
+          <IconButton>
+            <DeleteIcon />
+          </IconButton>
+        </Td>
       </Tr>
     );
   };
@@ -34,6 +54,8 @@ export default function Inventory() {
             <Th>SKU</Th>
             <Th>Location</Th>
             <Th>Count</Th>
+            <Th>Update</Th>
+            <Th>Delete</Th>
           </Tr>
         </Thead>
         <Tbody>
